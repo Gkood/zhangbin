6007d7b26e895ae56293f860e5c35388327459ef
Gkood <979987792@qq.com>
2016-12-27 10:56:13 +0800

    Add files via upload

---
+++ index/index.js
@@ -0,0 +1,17 @@
+define([
+	'jquery',
+	'template',
+	'proxy',
+	'layer',
+	'index/index.tpl',
+	'css!index/index.css'
+], function($, template, proxy, layer) {
+
+	//初始化
+	function init() {
+		
+	}
+	init();
+
+
+});
