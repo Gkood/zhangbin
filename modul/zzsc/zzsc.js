008ad01ba7b248aafdd5767dca0153fecad1dd05
Gkood <979987792@qq.com>
2016-12-29 15:03:20 +0800

    Update zzsc.js

--- modul/zzsc/zzsc.js
+++ modul/zzsc/zzsc.js
@@ -1,8 +1,8 @@
 $(function(){
 $('#zzsc').html('<canvas id="canvas"></canvas>');
 var WINDOW_WIDTH = 920;
-		var WINDOW_HEIGHT = 400;
-		var RADIUS = 7; //球半径
+		var WINDOW_HEIGHT = WINDOW_WIDTH;
+		var RADIUS = 8; //球半径
 		var NUMBER_GAP = 10; //数字之间的间隙
 		var u=0.65; //碰撞能量损耗系数
 		var context; //Canvas绘制上下文
